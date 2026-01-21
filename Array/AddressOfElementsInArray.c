f46968e198fbaf5a617b10d9e3d1c2412aad9cbb
Manas Gantait <manasgantait11@gmail.com>
2026-01-21 21:44:45 +0530

    modified one array problem

--- Array/AddressOfElementsInArray.c
+++ Array/AddressOfElementsInArray.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
     int arr[4] = {2,3,4,5};
-    for (int i = 0; i <= 3; i++)
+    for (int i = 0; i <= 3; i++) // loop is starting from here  
     {
         printf("%p\n",&arr[i]);
     }
